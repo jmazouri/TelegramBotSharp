d495ed7925aea5bbb75ea6e09690c57fd70e67b5
John Mazouri <jmazouri@users.noreply.github.com>
2015-06-25 01:30:29 -0400

    Began work on models

---
+++ TelegramBotSharp/TelegramBot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace TelegramBotSharp
+{
+    public class TelegramBot
+    {
+        public string AuthToken { get; private set; }
+        private RestClient _client;
+
+        public string ApiUrl
+        {
+            get { return "https://api.telegram.org/bot/" + AuthToken; }
+        }
+
+        public TelegramBot(string authToken)
+        {
+            if (String.IsNullOrWhiteSpace(authToken))
+            {
+                throw new ArgumentNullException("authToken", "Your authtoken must be valid. Message @Botfather on Telegram to get one.");
+            }
+
+            AuthToken = authToken;
+            _client = new RestClient(ApiUrl);
+
+        }
+    }
+}
---
+++ TelegramBotSharp/Types/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotSharp.Types
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public User From { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
